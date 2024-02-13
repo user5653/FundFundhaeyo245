33da0ccfab78161c491a9d6bf16858c8b045e5c2
Jaeyeong Kim <kjyyy7341@gmail.com>
2024-02-14 07:28:23 +0900

    맵핑 수정

--- fundfundhaeyo/src/main/java/com/kh/semi1st/company/controller/MoveToCompanyIntroductionController.java
+++ fundfundhaeyo/src/main/java/com/kh/semi1st/company/controller/MoveToCompanyIntroductionController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class moveToCompanyIntroductionController
  */
-@WebServlet("/introduction")
+@WebServlet("/introduction.cp")
 public class MoveToCompanyIntroductionController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
